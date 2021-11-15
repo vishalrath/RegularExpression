d168166c15767c672b5dc35cba444d059d31c810
Vishal@7038 <vishalrathod7038@gmail.com>
2021-11-15 17:27:31 +0530

    [vishal] Regular Expression Pattern for FirstName

---
+++ RegEx_FirstNameValidation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace RegEx_FirstNameValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            RegExUc1 registration = new RegExUc1();
+            registration.FirstName();
+
+            Console.WriteLine("Welcome to User Registration");
+            Console.WriteLine("\n Please enter  First Name--");
+            string firstName = Console.ReadLine();
+            RegExUc1.FirstName(firstName);                                          //taking user input and passing it in first name method of regular exp class
+            Console.ReadLine();
+        }
+    }
+}
---
+++ RegEx_FirstNameValidation/RegExUc1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegEx_FirstNameValidation
+{
+    class RegExUc1
+    {
+        public static void FirstName(string name)                   //declair para method
+        {
+            string firstName = "^[A-Z]{1}[a-zA-Z]{2,}$";            //this regular exp shows that first letter must be capital, and minimum letter must be 3
+            Regex regex = new Regex(firstName);
+            if (regex.IsMatch(name))                               //calling the Regex class's functions
+            {
+                Console.WriteLine(name + " is valid");
+            }
+            else
+                Console.WriteLine(name + " is invalid. Please check your name amd start is capital letter.");
+        }
+
+        string[] inputs = { "visha", "Vishal", "Vis", "XYz", "Abc" };
+        string pattern = "^[A-Z]{1}[a-zA-Z]{2,}$";            //this regular exp shows that first letter must be capital, and minimum letter must be 3
+
+        public void FirstName()
+        {
+            Regex regex = new Regex(pattern);
+            Console.WriteLine(" First Name Validation ");
+            foreach (string word in inputs)
+            {
+                if (regex.IsMatch(word))
+                {
+                    Console.WriteLine(word + "---> valid");
+                }
+                else
+                {
+                    Console.WriteLine(word + "---> invalid");
+                }
+            }
+
+        }
+    }
+}
