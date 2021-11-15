b4e5539695c346e1435a67c0465365d4bc3aa42a
Vishal@7038 <vishalrathod7038@gmail.com>
2021-11-15 19:58:45 +0530

    [vishal] RegEx Pattern for Password vallidation adding 1 number

---
+++ RegEx_PasswordVallidation3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace RegEx_PasswordVallidation3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            RegExPattern registration = new RegExPattern();
+            registration.FirstName();
+
+            Console.WriteLine("Welcome to User Registration");
+            Console.WriteLine("\n Please enter  First Name--");
+            string firstName = Console.ReadLine();
+            RegExPattern.FirstName(firstName);                                          //taking user input and passing it in first name method of regular exp class
+            Console.ReadLine();
+
+            Console.WriteLine("Hello World!");
+            RegExPattern registration1 = new RegExPattern();
+            registration.LastName();
+
+            RegExPattern mobile = new RegExPattern();
+            mobile.Validation();
+            Console.ReadLine();
+            RegExPattern Mail = new RegExPattern();
+            mobile.Validation();
+
+            RegExPattern Pass1 = new RegExPattern();
+            Pass1.PAsswordValidation();
+
+
+            RegExPattern Pass2 = new RegExPattern();
+            Pass2.PAsswordValidation2();
+            RegExPattern Pass3 = new RegExPattern();
+            Pass2.PAsswordValidation3();
+        }
+    }
+}
